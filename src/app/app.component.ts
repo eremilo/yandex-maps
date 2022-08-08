ba471f272213b3f0e0867b3029a5c0ee2ecdddad
valeriy.eremeev <eremeev@ipnote.pro>
2022-08-08 19:24:37 +0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,11 @@
 import { Component } from '@angular/core';
+import { YaEvent } from 'angular8-yandex-maps';
+
+interface Placemark {
+  geometry: number[];
+  properties: ymaps.IPlacemarkProperties;
+  options: ymaps.IPlacemarkOptions;
+}
 
 @Component({
   selector: 'app-root',
@@ -6,5 +13,70 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'yandex-maps';
+  chosenBalloon: any;
+  placemarks: Placemark[] = [];
+  coords: any;
+  newBalloon: any
+  ngOnInit() {
+    const testLocation: Placemark = {
+      geometry: [55.99673422345336, 37.21478035583862],
+      properties: {hintContent: 'информация о точке', balloonContent: 'Торговый центр'},
+      options: {
+        preset: 'islands#greenDotIcon',
+      }
+    };
+    // @ts-ignore
+    const coordinates = JSON.parse(localStorage.getItem('coordinates'));
+    this.placemarks = coordinates ? coordinates : [];
+    this.placemarks.push(testLocation);
+  }
+
+  onMapClick(e: YaEvent<ymaps.Map>): void {
+    const {target, event} = e;
+    if (!target.balloon.isOpen()) {
+      this.newBalloon = target
+      this.coords = event.get('coords');
+      target.balloon.open(this.coords, {
+        contentHeader: 'Добавьте информацию о точке',
+        contentBody:
+          '<p>Информация: <input type="text" id="balloon" ></p>',
+      });
+    } else {
+      target.balloon.close();
+      this.newBalloon = null;
+    }
+  }
+
+  addPoint(): void {
+    // @ts-ignore
+    let data = document.getElementById('balloon').value;
+    this.placemarks.push({
+      geometry: this.coords,
+      properties: {
+        hintContent: 'Информация о точке',
+        balloonContent: data,
+      },
+      options: {
+        preset: 'islands#greenDotIcon',
+      },
+    });
+    localStorage.setItem('coordinates', JSON.stringify(this.placemarks));
+    this.newBalloon.balloon.close()
+  }
+
+  deletePoint(): void {
+    this.placemarks = this.placemarks.filter(item => {
+      return item.geometry !== this.chosenBalloon;
+    });
+    this.chosenBalloon = null;
+    localStorage.setItem('coordinates', JSON.stringify(this.placemarks));
+  }
+
+  actionClick($event: YaEvent<ymaps.Placemark>) {
+    this.chosenBalloon = $event.target.geometry?.getCoordinates();
+  }
+
+  onMapBalloonClose() {
+    this.newBalloon = null;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { AngularYandexMapsModule, YaConfig } from 'angular8-yandex-maps';
+const mapConfig: YaConfig = {
+  apikey: '69893404-4a37-4423-bde6-79fb80684c2a',
+  lang: 'en_US',
+};
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularYandexMapsModule.forRoot(mapConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
